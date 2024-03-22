f289fd166253485adf8e925e5e6f6d51f6322a47
Danrylus <danrylus@gmail.com>
2024-03-22 13:53:05 -0500

    Agregado descifrado de contraseña

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
-const cors = require('cors'); // Importa el módulo cors
+const crypto = require('crypto');
+const cors = require('cors');
 
 const app = express();
 const PORT = 8760;
 
 app.use(express.json());
-app.use(cors()); // Habilita CORS para todas las solicitudes
+app.use(cors());
 
 // Configurar la conexión a la base de datos
 const db = mysql.createConnection({
@@ -28,6 +29,11 @@ app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
+// Función para generar un hash de contraseña con un salt fijo
+function hashPassword(password) {
+  const salt = 'salt_fijo_conocido'; // Salt fijo conocido
+  return crypto.createHash('sha256').update(password + salt).digest('hex');
+}
 
 // Endpoint para el inicio de sesión
 app.post('/login', (req, res) => {
@@ -37,13 +43,20 @@ app.post('/login', (req, res) => {
   db.query('SELECT * FROM cdx_users WHERE user_login = ?', [username], async (error, results) => {
     if (error) {
       console.log(error);
-      res.status(500).send('Error en el servidor' + error);
+      res.status(500).send('Error en el servidor');
     } else if (results.length === 0) {
       res.status(401).send('Credenciales inválidas');
     } else {
       const user = results[0];
-      res.status(401).send('Credenciales inválidas' + user);
-      // Aquí puedes continuar con la lógica para generar y devolver el token JWT
+      const hashedPassword = hashPassword(password);
+
+      if (hashedPassword === user.password) {
+        // Contraseña coincidente, generar y devolver el token JWT
+        const token = jwt.sign({ userId: user.id }, 'secreto_del_token');
+        res.json({ token });
+      } else {
+        res.status(401).send('Credenciales inválidas');
+      }
     }
   });
 });
@@ -51,4 +64,3 @@ app.post('/login', (req, res) => {
 app.listen(8760, () => {
   console.log(`Servidor Express corriendo en el puerto ${PORT}`);
 });
-
